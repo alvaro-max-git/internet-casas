10f24b754492e85aed1fd17caa4de47f9c9834cf
Alvaro Muñoz Pavón <alvaro.munoz.pavon@alumnos.upm.es>
2025-03-13 16:44:03 +0100

    Demo funcional básica, abre front door y extrae los datos de las cerraduras virtuales.

--- demoseam/seam-demo/src/main/java/com/demoseam/Main.java
+++ demoseam/seam-demo/src/main/java/com/demoseam/Main.java
@@ -1,28 +1,48 @@
 package com.demoseam;
 
-import java.io.Console;
-import java.util.*;
-import com.fasterxml.jackson.annotation.*;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import java.util.List;
+
 import com.seam.api.Seam;
-import com.seam.api.core.ObjectMappers;
-import com.seam.api.types.Device;
-import com.seam.api.types.Manufacturer;
-import com.seam.api.types.ActionAttempt;
-import com.seam.api.resources.devices.requests.DevicesListRequest;
+import com.seam.api.resources.locks.LocksClient;
 import com.seam.api.resources.locks.requests.LocksUnlockDoorRequest;
+import com.seam.api.types.ActionAttempt;
+import com.seam.api.types.Device;
+
 
 public class Main {
 
     public static void main(String[] args) {
   
     String SEAM_API_KEY = System.getenv("SEAM_API_KEY");
+    System.out.println("SEAM_API_KEY: " + SEAM_API_KEY);
+    
+    // If the environment variable is not found, use a default value
+    if (SEAM_API_KEY == null) {
+        SEAM_API_KEY = "seam_testKpVb_t1Qiw82sFJRv31ihiqjnVuQh"; // Your API key from CMD
+        System.out.println("Using hardcoded API key: " + SEAM_API_KEY);
+    }
 
-      Seam seam = Seam.builder()
-        .apiKey(SEAM_API_KEY)
-        .build();
-  
-      // Retrieve all devices, filtered by manufacturer,
+    Seam seam = Seam.builder()
+      .apiKey(SEAM_API_KEY)
+      .build();
+
+   System.out.println("Seam: " + seam);
+
+    LocksClient locksClient = seam.locks();
+    System.out.println("LocksClient: " + locksClient);
+
+    List<Device> allDevices = locksClient.list();
+    System.out.println("allDevices: " + allDevices);
+
+    Device frontDoor = allDevices.get(0);
+
+    ActionAttempt actionAttempt = seam.locks()
+          .unlockDoor(LocksUnlockDoorRequest.builder()
+            .deviceId(frontDoor.getDeviceId())
+            .build());
+            
+    /* 
+     // Retrieve all devices, filtered by manufacturer,
       // which is one of several filters that list() supports.
       var allAugustLocks = seam.devices().list(DevicesListRequest.builder()
         .manufacturer(Manufacturer.AUGUST)
@@ -31,10 +51,13 @@ public class Main {
       // Select the first device as an example.
       Device frontDoor = allAugustLocks.get(0);
       
+
+
       // Confirm that the device can remotely unlock.
       // You're using a capability flag here!
       if (frontDoor.getCanRemotelyUnlock())
       {
+        
         // Perform the unlock operation
         // and return an action attempt.
         ActionAttempt actionAttempt = seam.locks()
@@ -42,5 +65,7 @@ public class Main {
             .deviceId(frontDoor.getDeviceId())
             .build());
       }
-    }
+    */
+   
   }
+}
