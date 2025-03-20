5a907f9e95db5056332f8e2591925f183fe0d167
Alvaro Muñoz <alvaro.munoz.pavon@alumnos.upm.es>
2025-03-20 22:02:02 +0100

    Creado el proyecto de Spring Boot ioh-api para el backend, y editado application.properties como en la práctica.

---
+++ ioh-api/src/main/java/es/upm/dit/isst/ioh_api/IohApiApplication.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.ioh_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IohApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IohApiApplication.class, args);
+	}
+
+}
---
+++ ioh-api/src/test/java/es/upm/dit/isst/ioh_api/IohApiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.ioh_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IohApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
