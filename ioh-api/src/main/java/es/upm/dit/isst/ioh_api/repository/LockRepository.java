0b578f8537491cbf718043bd98f4bd560e2034d2
Alvaro Muñoz Pavón <alvaro.munoz.pavon@alumnos.upm.es>
2025-03-28 13:04:17 +0100

    Mejoras en los endpoints

--- ioh-api/src/main/java/es/upm/dit/isst/ioh_api/repository/LockRepository.java
+++ ioh-api/src/main/java/es/upm/dit/isst/ioh_api/repository/LockRepository.java
@@ -1,8 +1,16 @@
 package es.upm.dit.isst.ioh_api.repository;
 
 import es.upm.dit.isst.ioh_api.model.Lock;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface LockRepository extends CrudRepository<Lock, String> {
     // p.ej. List<Lock> findByPropietarioEmail(String email);
+
+    //hacer findByPropietario para obtener los locks de un usuario
+
+    List<Lock> findByPropietarioEmail(String email);
+    
 }
